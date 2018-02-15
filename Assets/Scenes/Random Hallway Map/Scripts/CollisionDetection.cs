789cc7dac3b9b5fe40e622ac711b6e0e727e1d76
carl0545 <carl0545@vandals.uidaho.edu>
2018-02-14 23:27:56 -0800

    Implemented Collision Detection and a Finish Line

---
+++ Assets/Scenes/Random Hallway Map/Scripts/CollisionDetection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour {
+
+	public static int totalCollision = 0;
+
+	void OnTriggerEnter(Collider col){
+
+		//Destroy(col.gameObject);
+		if(col.gameObject.name == "Cube(Clone)" || col.gameObject.name == "Sphere(Clone)"){
+			//Debug.Log("COLLISION");
+			//Destroy(col.gameObject);
+			totalCollision++;
+			Debug.Log("Total collision: ");
+			Debug.Log(totalCollision);
+		}
+
+
+	}
+
+}
---
+++ Assets/Scenes/Random Hallway Map/Scripts/FinishLine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col){
+
+
+		if(col.gameObject.name == "FinishLine"){
+			Debug.Log("HI THERE");
+			Application.Quit();
+
+		}
+
+
+	}
+}

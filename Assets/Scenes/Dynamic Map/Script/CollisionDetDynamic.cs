cb61c1ac513aec4c83ad6eed4e5ebd9230455d25
carl0545 <carl0545@vandals.uidaho.edu>
2018-02-22 11:51:59 -0800

    Fixed bugs with the finish line

---
+++ Assets/Scenes/Dynamic Map/Script/CollisionDetDynamic.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class CollisionDetDynamic : MonoBehaviour {
+
+	public static int totalCollision = 0; //Keeps track of total amount of collision
+
+	/*
+	* Function detects when a character hits an obstacle and increments totalCollisions
+	*/
+	void OnTriggerEnter(Collider col){
+
+		if(col.gameObject.name == "Cube(Clone)" || col.gameObject.name == "Sphere(Clone)" ||
+				col.gameObject.name == "Ground" || col.gameObject.name == "Right Wall" ||
+				col.gameObject.name == "Left Wall" || col.gameObject.name == "Back Wall" ||
+				col.gameObject.name == "Front Wall"){
+			totalCollision++;
+			Debug.Log(totalCollision);
+		}
+	}
+
+}

44619bf522dc3b7686a52cca47911e244831b984
carl0545 <carl0545@vandals.uidaho.edu>
2018-03-26 16:53:02 -0700

    Collisions now require an time interval before they count

--- Assets/Scenes/Random Hallway Map/Scripts/CollisionDetection.cs
+++ Assets/Scenes/Random Hallway Map/Scripts/CollisionDetection.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CollisionDetection : MonoBehaviour {
 
 	public static int totalCollision = 0; //Keeps track of total amount of collision
+	private static float lastCollisionTime = 0f; //Holds the time of the previous collision
+	private const float MAX_COLLISION_INTERVAL = 1f; //Maximum interval to count another collision
 
 	/*
 	* Function detects when a character hits an obstacle and increments totalCollisions
@@ -15,22 +17,43 @@ public class CollisionDetection : MonoBehaviour {
 				col.gameObject.name == "Ground" || col.gameObject.name == "Right Wall" ||
 				col.gameObject.name == "Left Wall" || col.gameObject.name == "Back Wall" ||
 				col.gameObject.name == "Front Wall"){
-			totalCollision++;
-			Debug.Log(totalCollision);
+			AddToTotalCollisions();
 		}
 
-		/*
-		if (col.gameObject.tag == "Collidable") {
-			totalCollision++;
-			Debug.Log (totalCollision);
-		}
-		*/
 	}
 
+	/*
+	*Checks to see if Collisions are separated by MAX_COLLISION_INTERVAL before adding to totalCollision
+	*/
+	private void AddToTotalCollisions(){
+		if(lastCollisionTime == 0.0){
+			lastCollisionTime = Time.time;
+			totalCollision++;
+			return;
+		}
+
+		float currTime, netTime;
+
+		currTime = Time.time;
+		netTime = currTime - lastCollisionTime;
+
+		if(netTime > MAX_COLLISION_INTERVAL){
+			totalCollision++;
+		}
+
+		lastCollisionTime = currTime;
+
+	}
+
+	/*
+	*Gets the total collisions
+	*/
 	public int GetTotalCollisions(){
 		return totalCollision;
 	}
-
+	/*
+	*Resets the totalCollision variable
+	*/
 	public void ResetTotalCollisions(){
 		totalCollision = 0;
 	}

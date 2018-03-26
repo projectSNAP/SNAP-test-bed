60675edbcc26c5a027f31935271c39535b78fe25
carl0545 <carl0545@vandals.uidaho.edu>
2018-03-26 16:32:51 -0700

    Total Collisions now reset at the end of every test. Still bugs on colls

--- Assets/Scenes/Random Hallway Map/Scripts/CollisionDetection.cs
+++ Assets/Scenes/Random Hallway Map/Scripts/CollisionDetection.cs
@@ -10,23 +10,29 @@ public class CollisionDetection : MonoBehaviour {
 	* Function detects when a character hits an obstacle and increments totalCollisions
 	*/
 	void OnTriggerEnter(Collider col){
-		/*
+
 		if(col.gameObject.name == "Cube(Clone)" || col.gameObject.name == "Sphere(Clone)" ||
 				col.gameObject.name == "Ground" || col.gameObject.name == "Right Wall" ||
 				col.gameObject.name == "Left Wall" || col.gameObject.name == "Back Wall" ||
 				col.gameObject.name == "Front Wall"){
 			totalCollision++;
-			//Debug.Log(totalCollision);
+			Debug.Log(totalCollision);
 		}
-		*/
+
+		/*
 		if (col.gameObject.tag == "Collidable") {
 			totalCollision++;
 			Debug.Log (totalCollision);
 		}
+		*/
 	}
 
 	public int GetTotalCollisions(){
 		return totalCollision;
+	}
+
+	public void ResetTotalCollisions(){
+		totalCollision = 0;
 	}
 
 }

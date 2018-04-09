2d96dea8b9d24aa2f63bd6e46ca80c10db4731a0
Andrew Rose <rose0531@vandals.uidaho.edu>
2018-04-08 18:56:20 -0600

    Get frame from Unity and pass it to shared memory space

--- Assets/SharedMemory.cs
+++ Assets/SharedMemory.cs
@@ -13,17 +13,13 @@ public class SharedMemory : MonoBehaviour {
 	static extern IntPtr ReadArrayFromSharedMemory(ref int size);
 
 	Texture2D tex;
-	Texture2DArray texArr;
 	public Camera cam;
+	bool capture;
 
 	void Start(){
-		cam.depthTextureMode = DepthTextureMode.Depth;
-		tex = new Texture2D(cam.pixelWidth, cam.pixelHeight, TextureFormat.RGBA32, false); //takes a screen shot from the cameras perspective
-		byte[] bytes = tex.GetRawTextureData ();
-		int[] intArr = ConvertToIntArray (bytes);
-		Debug.Log ("intArr: " + intArr[0] + ", " + intArr[1] + ", " + intArr[2] + ", " + intArr[3]);
-
-
+		//cam.depthTextureMode = DepthTextureMode.Depth;
+		capture = false;
+		/*
 		//Initialize an IntPtr for our int array that we want to write to memory
 		GCHandle handle = GCHandle.Alloc (intArr, GCHandleType.Pinned);
 		IntPtr ipArr = handle.AddrOfPinnedObject();
@@ -41,8 +37,10 @@ public class SharedMemory : MonoBehaviour {
 			Debug.Log ("Write to shared memory failed!");
 		else
 			Debug.Log ("Write SUCCESS!");
-		
 
+		*/
+		
+		/*
 		//Read the array from memory and store it in an IntPtr
 		int npArrSize = 0;
 		IntPtr npArr = ReadArrayFromSharedMemory (ref npArrSize);
@@ -57,8 +55,72 @@ public class SharedMemory : MonoBehaviour {
 		Marshal.Copy (npArr, nArr, 0, npArrSize);
 
 		Debug.Log ("nArr: " + nArr[0] + ", " + nArr[1] + ", " + nArr[2] + ", " + nArr[3]);
+		*/
 
 	}
+
+	void Update(){
+		if (Input.GetKey (KeyCode.C)) {
+			capture = true;
+		}
+	}
+
+	void OnPostRender(){
+		if (capture) {
+			cam.depthTextureMode = DepthTextureMode.Depth;
+			int width = cam.pixelWidth;
+			int height = cam.pixelHeight;
+			tex = new Texture2D (width, height, TextureFormat.RGBA32, false);
+			tex.ReadPixels (new Rect (0, 0, width, height), 0, 0);
+			tex.Apply ();
+			byte[] bytes = tex.GetRawTextureData ();
+			Destroy (tex);
+			Debug.Log ("bytes: " + bytes [0] + ", " + bytes [1] + ", " + bytes [2] + ", " + bytes [bytes.Length - 3] + ", " + bytes [bytes.Length - 2] + ", " + bytes [bytes.Length - 1]);
+			Debug.Log ("bytes len: " + bytes.Length);
+			//System.IO.File.WriteAllBytes (Application.dataPath + ("/testPIC"), bytes);
+			int[] intArr = ConvertToIntArray (bytes);
+			Debug.Log ("intArr: " + intArr [0] + ", " + intArr [1] + ", " + intArr [2] + ", " + intArr [3] + ", " + intArr[intArr.Length - 3] + ", " + intArr[intArr.Length - 2] + ", " + intArr[intArr.Length - 1]);
+			Debug.Log ("intArr size: " + intArr.Length);
+
+			//Initialize an IntPtr for our int array that we want to write to memory
+			GCHandle handle = GCHandle.Alloc (intArr, GCHandleType.Pinned);
+			IntPtr ipArr = handle.AddrOfPinnedObject();
+
+
+			//Copy our int array to our IntPtr that we initialized
+			Marshal.Copy (intArr, 0, ipArr, intArr.Length); 	//(int[] source, int startIndex, IntPtr dest, int length)
+
+
+			//Write the IntPtr array to memory
+			int err = WriteArrayToSharedMemory (ipArr, intArr.Length);
+
+			if (err != 0)
+				Debug.Log ("Write to shared memory failed!");
+			else
+				Debug.Log ("Write SUCCESS!");
+
+			/*
+			//Read the array from memory and store it in an IntPtr
+			int npArrSize = 0;
+			IntPtr npArr = ReadArrayFromSharedMemory (ref npArrSize);
+			if (npArr == null)
+				Debug.Log ("Read from shared memory failed!");
+			else
+				Debug.Log ("Read SUCCESS!");
+
+
+			//Copy elements that the IntPtr points to over to a new int array
+			int[] nArr = new int[npArrSize];
+			Marshal.Copy (npArr, nArr, 0, npArrSize);
+
+			Debug.Log ("nArr: " + nArr[0] + ", " + nArr[1] + ", " + nArr[2] + ", " + nArr[3] + ", " + nArr[nArr.Length - 3] + ", " + nArr[nArr.Length - 2] + ", " + nArr[nArr.Length - 1]);
+			Debug.Log ("nArr size: " + nArr.Length);
+			*/
+
+			capture = false;
+		}
+	}
+
 
 	int[] ConvertToIntArray(byte[] bytes){
 		int[] intArr = new int[bytes.Length];

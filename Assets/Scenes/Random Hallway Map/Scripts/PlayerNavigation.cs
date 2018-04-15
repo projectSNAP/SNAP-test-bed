80f0080ed888eac03888f418182be4218f814fd3
carl0545 <carl0545@vandals.uidaho.edu>
2018-04-15 13:07:41 -0700

    Script now checks player direction to determine if headed N/S

--- Assets/Scenes/Random Hallway Map/Scripts/PlayerNavigation.cs
+++ Assets/Scenes/Random Hallway Map/Scripts/PlayerNavigation.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class PlayerNavigation : MonoBehaviour {
 
+	public AudioSource north;
+	public AudioSource south;
+
+	public GameObject player;
+
+	private float playerDir;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		playerDir = player.transform.eulerAngles.y;
+
+
+		if (Input.GetKeyDown("p")){
+			Debug.Log(playerDir);
+			if ((playerDir > 270f && playerDir <= 360f) || (playerDir > 0f && playerDir < 90f)){
+				north.Play();
+			}
+			else if((playerDir < 270f && playerDir > 180f) || (playerDir > 90f && playerDir < 180f)){
+				south.Play();
+			}
+		}
+
 	}
 }

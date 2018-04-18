951476f6f6b9527afd0a5e1e342b9861bc0a786a
Andrew Rose <rose0531@vandals.uidaho.edu>
2018-04-17 19:11:09 -0600

    reorganized files, removed redundant files, added visual audio engine to unity

---
+++ Assets/CharacterController/DepthCopy/SharedMemory.cs
@@ -0,0 +1,85 @@
+﻿/*	Video explaining how Shaders work:
+ * 		https://www.youtube.com/watch?v=C0uJ4sZelio&t=1799s
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System; //allows us to use IntPtr type
+using System.Runtime.InteropServices; //allows us to access our SharedMemTestLib.dll
+using System.IO;
+using System.Diagnostics; //allows us to use Process
+
+
+public class SharedMemory : MonoBehaviour {
+	[DllImport("bin\\SharedMemoryDLL.dll")]
+	static extern IntPtr CreatePointerToSharedMemory (int x, int y);
+
+	[DllImport("bin\\SharedMemoryDLL.dll")]
+	static extern void UnmapPointerToSharedMemory (IntPtr pSharedMem);
+
+
+
+	public Material mat;
+	int width, height;
+	Texture2D tex;
+	string pathToVAEExecutable;
+	Process process;
+	byte[] bytes;
+	IntPtr ptrToSharedMemory;
+	const int RGBA_TYPE = 4;
+	bool processRunning = false;
+	Camera cam;
+
+	void Start () {
+		cam = GetComponent<Camera> ();
+		cam.depthTextureMode = DepthTextureMode.Depth;
+		width = cam.pixelWidth;
+		height = cam.pixelHeight;
+		tex = new Texture2D(width, height, TextureFormat.RGBA32, false);
+
+		print (Application.dataPath);
+		pathToVAEExecutable =  Application.dataPath + "SNAP-visual-audio-engine.exe";
+
+		if (processRunning) {
+			process.Kill ();
+		}
+		process = System.Diagnostics.Process.Start (pathToVAEExecutable);
+		processRunning = true;
+	}
+
+	void OnEnable()
+	{
+		cam = GetComponent<Camera> ();
+		cam.depthTextureMode = DepthTextureMode.Depth;
+		width = cam.pixelWidth;
+		height = cam.pixelHeight;
+		bytes = new byte[RGBA_TYPE * width * height];
+		ptrToSharedMemory = CreatePointerToSharedMemory(width, height);
+	}
+
+
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		//src is the fully rendered scene that you would normally
+		//send to the monitor. We are intercepting this so we can
+		//modify it before passing it on.
+		Graphics.Blit(source, destination, mat);
+		RenderTexture.active = destination;
+
+		//Reads pixels from the camera to our texture2D
+		tex.ReadPixels(new Rect(0, 0, width, height), 0, 0, false);
+		tex.Apply();
+		bytes = tex.GetRawTextureData();
+
+		//Copy our byte array to our shared memory space
+		Marshal.Copy (bytes, 0, ptrToSharedMemory, bytes.Length);
+	}
+
+	void OnDisable()
+	{
+		UnmapPointerToSharedMemory (ptrToSharedMemory);
+		process.Kill();
+	}
+
+}
---
+++ Assets/Menu/LogButtonList.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class LogButtonList : MonoBehaviour {
+
+	#region Singleton
+	public static LogButtonList instance;
+
+	void Awake(){
+		instance = this;
+	}
+	#endregion
+
+	public List<GameObject> logsList = new List<GameObject> ();
+	public GameObject logButton; /*holds my "LogButton" prefab*/
+	public Transform grid;
+
+	// Use this for initialization
+	void Start () {
+		RefreshDisplay ();
+	}
+
+	public void RefreshDisplay(){
+		AddLogButton ();
+	}
+
+	private void AddLogButton(){
+		/*For each log file in the "Logs" folder, we need to create a button*/
+		int logFileNumber = 0;
+		string logFilePath;
+		/*While loop seaches through "Logs" folder and checks if the log file, with a specific number, exists*/
+		while (System.IO.File.Exists ("Assets/Logs/log" + logFileNumber + ".json")) {
+			/*Path to the log file*/
+			logFilePath = "Assets/Logs/log" + logFileNumber + ".json";
+
+			/*Create a new log button from "LogButton" prefab. (Assets->UX Prefabs)*/
+			GameObject newLogButton = Instantiate(logButton);
+
+			/*Place the button in the "Grid" Transform component*/
+			newLogButton.transform.SetParent (grid);
+
+			/*Creates an instance of our LogButtonScript class using our newLogButton*/
+			LogButtonScript informationLogButton = newLogButton.GetComponent<LogButtonScript> ();
+
+			/*Access the "Setup" function from out LogButtonScript class, which
+			 *takes the information from the log file and puts it in our button.
+			 */
+			informationLogButton.Setup (logFilePath);
+
+			/*Add the log button to our log button list*/
+			logsList.Add (newLogButton);
+
+			logFileNumber++;
+		}
+
+	}
+
+
+	public void Add(GameObject log){
+		logsList.Add (log);
+	}
+
+	public void Filter(string mapName){
+		foreach(GameObject log in logsList){
+			if (log.GetComponent<LogButtonScript> ().mapLabel.text != mapName) {
+				log.SetActive (false);
+			} else {
+				log.SetActive (true);
+			}
+		}
+	}
+
+	public void ResetFilter(){
+		foreach(GameObject log in logsList){
+			log.SetActive (true);
+		}
+	}
+}

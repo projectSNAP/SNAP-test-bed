7c95b696c0f2f4f0dce02b8d9150b8eddb112364
carl0545 <carl0545@vandals.uidaho.edu>
2018-04-19 13:34:10 -0700

    Working on probably broken system

---
+++ Assets/UX Prefabs/StarRating.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarRating : MonoBehaviour {
+
+	private RawImage oldImage;
+	private RawImage newImage;
+
+	// Use this for initialization
+	void Start () {
+    oldImage = GetComponent<RawImage>();
+		newImage.texture = Resources.Load("filled_star");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnStarClick() {
+
+
+	}
+}

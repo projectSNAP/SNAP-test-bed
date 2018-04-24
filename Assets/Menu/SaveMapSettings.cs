9aa14a3200dc2720f0848d97b8504af9bfd87f5a
carl0545 <carl0545@vandals.uidaho.edu>
2018-04-24 12:05:03 -0700

    Created MapSettings class

---
+++ Assets/Menu/SaveMapSettings.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSettings{
+	public int cubesSpawned;
+	public int spheresSpawned;
+	public int cubeMinSize;
+	public int sphereMinSize;
+	public int cubeMaxSize;
+	public int sphereMaxsize;
+	public int mapSelected; //0-hallway 1-Dynamic
+	public bool vision;
+}

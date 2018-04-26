3daa111acd75a3f72945aa5e53d3826305d1d3bd
Andrew Rose <rose0531@vandals.uidaho.edu>
2018-04-25 18:01:15 -0600

    updated configuration settings to new configuration settings

--- Assets/Menu/SaveConfigurationSettings.cs
+++ Assets/Menu/SaveConfigurationSettings.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SaveConfigurationSettings{
-	public float savedFrequency;
-	public float savedFrequencyIncrement;
-	public float savedHorizontalSteps;
-	public float savedStepDelay;
-	public float savedAudioSpreadDegree;
-	public float savedAudioVolumeRollOff;
+	public float savedFrequencyMax;
+	public float savedFrequencyMin;
+	public int savedHorizontalResolution;
+	public int savedVerticalResolution;
+	public float savedFieldOfView;
+	public float savedSampleLength;
+	public int savedCycleLength;
 	public int savedScanningType;
+	public int savedDistanceIndicator;
+	public int savedHeightIndicator;
+	public int savedDepthLength;
 	public bool savedVision;
 }
